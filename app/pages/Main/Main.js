a5190e946c384bf327dc970f8bdb6493c4070841
aaditya-thakkar <thakkar.aadi1@gmail.com>
2017-09-30 13:40:27 +0530

    added nav support on homepage

--- app/pages/Main/Main.js
+++ app/pages/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Login from '../../Components/Login';
 import Signup from '../../Components/Signup';
+import HomePage from '../../pages/HomePage';
 
 export default class Main extends React.Component {
     render() {
         return (
             <main>
                 <Switch>
+                    <Route exact path='/' component={HomePage} />
                     <Route path='/login' component={Login} />
                     <Route path='/signup' component={Signup} />
                 </Switch>
