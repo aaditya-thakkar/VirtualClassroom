39a16ea4bf8d28623d1ba73f849858658edf7a1a
aaditya-thakkar <thakkar.aadi1@gmail.com>
2017-10-03 16:46:30 +0530

    virtual classroom live video streaming added

---
+++ app/pages/HomePage/courses_helper.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+export default function() {
+    return axios.get('http://localhost:8081/courses')
+        .then(response => {
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -49,7 +49,7 @@ module.exports = function (app, db) {
     app.put('/users/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
-        const user = { email: req.body.email, password: req.body.password };
+        const user = { email: req.body.email, password: req.body.password, istutor: req.body.istutor };
         db.collection('users').updateOne(details, user, (err, result) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
@@ -59,5 +59,62 @@ module.exports = function (app, db) {
         });
     });
 
+    app.get('/courses/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('courses').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.get('/courses', (req, res) => {
+        db.collection('courses').find({}).toArray(function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(result);
+            }
+        });
+    });
+
+    app.post('/courses', (req, res) => {
+        const course = { id: req.body.id, name: req.body.name, liveChannel: req.body.liveChannel, tutorId: req.body.tutorId, image: req.body.image };
+        db.collection('courses').insertOne(course, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    app.put('/courses/:courseid', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const course = { id: req.body.id, name: req.body.name, liveChannel: req.body.liveChannel, tutorId: req.body.tutorId, image: req.body.image };
+        db.collection('courses').updateOne(details, course, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(course);
+            }
+        });
+    });
+
+    app.delete('/courses/:courseid', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('courses').removeOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send('User ' + courseid + ' deleted!');
+            }
+        });
+    });
 
 };
